8ef2e96081b959f9b688e54b03bb1abe6237750f
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2019-11-18 20:47:58 +0100

    "psp ex3 examen prueba"

--- src/psp_pruebaex_3/Monitor.java
+++ src/psp_pruebaex_3/Monitor.java
@@ -3,12 +3,14 @@ package psp_pruebaex_3;
 import static java.lang.Thread.sleep;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+//Como cada Hilo accede a un método sync distinto PERO todos acceden a una misma variable,
+//lo único que habrá que controlar será el acceso a la variable compartida (?)
 
 public class Monitor {
 
     //variable compartida
     private int ocurrencias = 0;
-
+    //semáforo
     private Boolean disp = true;
 
     public synchronized void contarMayus(String texto) {
@@ -26,6 +28,8 @@ public class Monitor {
         disp = false;
 
         //ACCEDER 10 VECES AL MÉTODO EN LA CLASE DEL HILO EN VEZ DE PONER AQUÍ EL FOR??
+        //Podríamos hacer que cada uno solo lea un caracter del texto en cada iteracción con él
+        //(cada hilo cuenta todos sus elementos y hasta que acabe no empieza otro, como si fuera un join )
         for (int i = 0; i < texto.length(); i++) {
 
             if (Character.isUpperCase(texto.charAt(i))) {
